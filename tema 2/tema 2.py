aa1e88fd49f7cd750d68aabf601ec892395a1e32
Alin56-afk <temianalinrazvan21@gmail.com>
2024-10-28 13:50:10 +0200

    tema 2

---
+++ tema 2/tema 2.py
@@ -0,0 +1,5 @@
+text="""(Muzeul Județean de Artă Centrul Artistic Baia Mare vă invită la un eveniment muzical în cadrul căruia ritmul, armonia și improvizația din jazz vor rezona din nou alături de compozițiile picturale, unde liniile, culorile și formele se vor alătura muzicii într-un dialog creativ inedit."""
+print (text)
+text_1=text [0:len(text)//2]
+text_2=text [len(text)//2:]
+>>>
